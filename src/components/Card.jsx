83d51dbc19146e145d86fed6431eafc00771aedd
Rodrigo <rodrigolfranca@gmail.com>
2022-05-16 16:32:50 -0300

    Agora só o CSS :D (   :(   )

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <div className='Card'>
+            <h3 className="data">{props.data}</h3>
+            <p className="local">{props.local}</p>
+            <span className="Compromisso">{props.appointment}</span>
+        </div>
+    );
+}
+
+export default Card;
--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Card from './Card';
 
 const CardsContainer = () => {
     return (
-        <div className="CardsContainer"></div>
+        <div className="CardsContainer">
+            <Card data="24.03.2022" local="Fortaleza - CE" appointment="Encontro com o Alpha" />
+            <Card data="24.03.2022" local="Fortaleza - CE" appointment="Reunião DM" />
+            <Card data="24.03.2022" local="Fortaleza - CE" appointment="Aula Faculdade" />
+        </div>
     );
 }
 
